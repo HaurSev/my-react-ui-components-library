e9c132ae6ebb131a3fda7f8eb02edc52d948983e
HaurSev <usevalad.haurylenka@innowise.com>
2025-09-22 09:57:39 +0300

    Delete .rollup.cache directory

--- .rollup.cache/D/Project/Стажировка/Reacts_components2/my-react-app/src/components/Modal/ModalContent.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import styles from './Modal.module.css';
-export var ModalContent = function (_a) {
-    var children = _a.children, _b = _a.className, className = _b === void 0 ? '' : _b;
-    return (_jsx("div", { className: "".concat(styles.modalContent, " ").concat(className), children: children }));
-};
-ModalContent.displayName = 'ModalContent';
-//# sourceMappingURL=ModalContent.js.map
